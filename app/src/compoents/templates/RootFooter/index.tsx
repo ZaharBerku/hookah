3c625638f20683e637347e3ec32d70434872e388
ZaharBerku <zaharberku@gmail.com>
2024-04-20 00:05:19 +0300

    added links to footer

--- app/src/compoents/templates/RootFooter/index.tsx
+++ app/src/compoents/templates/RootFooter/index.tsx
@@ -1,10 +1,12 @@
-import { Footer, Wrapper, Button } from "@/compoents/atoms";
+import { Footer, Wrapper, Button, List } from "@/compoents/atoms";
 import { Logo, SocialButton } from "@/compoents/molecules";
+
+import { navList } from "@/utils/mock";
 
 const RootFooter = () => {
   return (
     <Footer className="w-full bg-black">
-      <Wrapper className="py-6 md:py-10">
+      <Wrapper className="py-6 md:py-10 flex justify-between gap-2 md:gap-10">
         <div className="flex flex-col gap-4">
           <Logo />
           <Button
@@ -24,10 +26,24 @@ const RootFooter = () => {
             ibrahim.farouq@gmail.com
           </Button>
           <div className="flex gap-3 mt-4">
-            <SocialButton href={'#'} />
-            <SocialButton href={'#'} />
+            <SocialButton href={"#"} />
+            <SocialButton href={"#"} />
           </div>
         </div>
+        <List className="flex flex-wrap w-full gap-3 justify-end md:justify-between items-start">
+          {navList.map((item, index) => (
+            <List.Item key={index}>
+              <Button
+                color="transparent"
+                className="text-white text-base"
+                href={item.link}
+                as={"link"}
+              >
+                {item.name}
+              </Button>
+            </List.Item>
+          ))}
+        </List>
       </Wrapper>
     </Footer>
   );

e1e586a0ac6d7fd878c12a7bd79737a4af12dcc2
ZaharBerku <zaharberku@gmail.com>
2024-06-02 18:19:48 +0300

    create metaa data for hookah and  tobacco pages

--- app/src/app/[locale]/hookah/page.tsx
+++ app/src/app/[locale]/hookah/page.tsx
@@ -3,6 +3,7 @@ import { GET_CATEGORY_PRODUCTS_QUERY } from "@/query/schema";
 import { notFound } from "next/navigation";
 
 import { getClient } from "@/lib/server";
+import { locales } from "@/utils/navigation";
 
 export default async function Hookah() {
   const { loading, error, data } = await getClient().query({
@@ -22,4 +23,43 @@ export default async function Hookah() {
       brands={data.brands.data}
     />
   );
+}
+
+export async function generateMetadata({
+  params
+}: {
+  params: { locale: "uk" | "ru" };
+}) {
+  const locale = locales.includes(params.locale) ? params.locale : "uk";
+  const { Hookah } = await import(`../../../../messages/${locale}.json`);
+  return {
+    metadataBase: `${process.env.NEXT_PUBLIC_BASE_URL}/${locale}`,
+    title: Hookah.title,
+    description: Hookah.description,
+    openGraph: {
+      title: Hookah.ogTitle,
+      description: Hookah.ogDescription,
+      images: [
+        {
+          url: "https://strapi-hookah-images.s3.us-east-1.amazonaws.com/logo_b8a1bc1da6.png",
+          type: "image/png",
+          width: 150,
+          height: 150,
+          secureUrl:
+            "https://strapi-hookah-images.s3.us-east-1.amazonaws.com/logo_b8a1bc1da6.png"
+        },
+        {
+          url: "https://strapi-hookah-images.s3.us-east-1.amazonaws.com/logo_with_full_name_f0d133e35b.png",
+          type: "image/png",
+          width: 500,
+          height: 300,
+          secureUrl:
+            "https://strapi-hookah-images.s3.us-east-1.amazonaws.com/logo_with_full_name_f0d133e35b.png"
+        }
+      ],
+      type: "website",
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${locale}`,
+      locale: locale === "uk" ? "uk_UA" : "ru_RU"
+    }
+  };
 }

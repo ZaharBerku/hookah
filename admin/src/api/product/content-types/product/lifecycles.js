7a7c18334d1ff04a055720fd95453ef4b0d47ba0
ZaharBerku <zaharberku@gmail.com>
2024-06-03 23:52:00 +0300

    upgrade schema product

---
+++ admin/src/api/product/content-types/product/lifecycles.js
@@ -0,0 +1,29 @@
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = {
+  async beforeCreate(event) {
+    const latestProduct = await strapi.query("api::product.product").findOne({
+      orderBy: { odId: "desc" },
+    });
+
+    const newOdId = latestProduct ? latestProduct.odId + 1 : 1;
+    event.params.data.odId = newOdId;
+    event.params.data.compositeId = `${newOdId}-${event.params.data.slug}`;
+  },
+  async afterUpdate(event) {
+    if (
+      !event.params.data.compositeId &&
+      event.params.data.odId &&
+      event.params.data.slug
+    ) {
+      event.params.data.compositeId = `${event.params.data.odId}-${event.params.data.slug}`;
+    } else if (!event.params.data.compositeId) {
+      const latestProduct = await strapi.query("api::product.product").findOne({
+        orderBy: { odId: "desc" },
+      });
+      const newOdId = latestProduct.length > 0 ? latestProduct[0].odId + 1 : 1;
+      event.params.data.odId = newOdId;
+      event.params.data.compositeId = `${newOdId}-${event.params.data.slug}`;
+    }
+  },
+};

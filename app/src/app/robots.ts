dafa31671d2c7e8780c1dc2f9d23ef92a2536246
ZaharBerku <zaharberku@gmail.com>
2024-07-03 20:04:31 +0300

    return robots txt

--- app/src/app/robots.ts
+++ app/src/app/robots.ts
@@ -5,31 +5,31 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        // allow: [
-        //   "/uk",
-        //   "/ru",
-        //   "/uk/tobacco/",
-        //   "/uk/tobacco$",
-        //   "/ru/tobacco/",
-        //   "/ru/tobacco$",
-        //   "/uk/hookah/",
-        //   "/uk/hookah$",
-        //   "/ru/hookah/",
-        //   "/ru/hookah$"
-        // ],
-        // disallow: [
-        //   "/",
-        //   "404",
-        //   "500",
-        //   "/ru/search",
-        //   "/uk/search",
-        //   "/ru/cart",
-        //   "/uk/cart",
-        //   "/ru/cart/checkout",
-        //   "/uk/cart/checkout",
-        //   "/*?brands=",
-        //   "/*?search="
-        // ]
+        allow: [
+          "/uk",
+          "/ru",
+          "/uk/tobacco/",
+          "/uk/tobacco$",
+          "/ru/tobacco/",
+          "/ru/tobacco$",
+          "/uk/hookah/",
+          "/uk/hookah$",
+          "/ru/hookah/",
+          "/ru/hookah$"
+        ],
+        disallow: [
+          "/",
+          "404",
+          "500",
+          "/ru/search",
+          "/uk/search",
+          "/ru/cart",
+          "/uk/cart",
+          "/ru/cart/checkout",
+          "/uk/cart/checkout",
+          "/*?brands=",
+          "/*?search="
+        ]
       }
     ],
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`

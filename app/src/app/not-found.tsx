bf82f1034309ad0e9071466508826d8e46876d01
ZaharBerku <zaharberku@gmail.com>
2024-07-03 19:04:02 +0300

    remove new file

--- app/src/app/not-found.tsx
+++ app/src/app/not-found.tsx
@@ -1,11 +1,15 @@
-import { ErrorPage } from "@/compoents/pages";
+import { Typography } from "@/compoents/atoms";
+import Image from "next/image";
 
 import RootLayout from "@/app/[locale]/layout";
 
-export default function NotFound() {
+export default function Custom500() {
   return (
-    <RootLayout params={{ locale: "uk" }}>
-      <ErrorPage />
-    </RootLayout>
+    <div className="text-center flex flex-col justify-center items-center">
+      <div className="relative w-64 h-72">
+        <Image fill src={"/images/oops-grass-avatar.png"} alt="error-image" />
+      </div>
+      <Typography>Упппс... Щось пішло не так.</Typography>
+    </div>
   );
 }

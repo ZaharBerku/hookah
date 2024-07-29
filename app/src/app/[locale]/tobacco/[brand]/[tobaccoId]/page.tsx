27eaab66c58b75f6f67401c147fe67f1769b2005
ZaharBerku <zaharberku@gmail.com>
2024-07-29 17:51:33 +0300

    added logs

--- app/src/app/[locale]/tobacco/[brand]/[tobaccoId]/page.tsx
+++ app/src/app/[locale]/tobacco/[brand]/[tobaccoId]/page.tsx
@@ -1,10 +1,10 @@
 // import { TobaccoProductPage } from "@/compoents/pages";
 import { GET_TOBACCO_PRODUCT_BY_COMPOSITE_ID_QUERY } from "@/query/tobacco";
-// import { getTranslations } from "next-intl/server";
+import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 
 import { getQuery } from "@/lib/server";
-// import { getLocale } from "@/utils/helpers";
+import { getLocale } from "@/utils/helpers";
 
 export default async function TobaccoProduct({
   params
@@ -27,45 +27,49 @@ export default async function TobaccoProduct({
   );
 }
 
-// export async function generateMetadata({
-//   params
-// }: {
-//   params: { locale: "uk" | "ru"; tobaccoId: string };
-// }) {
-//   const locale = getLocale(params);
-//   const { data } = await getQuery({
-//     params,
-//     query: GET_TOBACCO_PRODUCT_BY_COMPOSITE_ID_QUERY,
-//     variables: {
-//       compositeId: params.tobaccoId
-//     }
-//   });
-//   const product = data.products.data?.at(0).attributes;
-//   const image = product.previewImage.data.attributes.url;
-//   const slugBrand = product.brand.data.attributes.slug;
-//   const t = await getTranslations({
-//     locale,
-//     namespace: "Tobacco.Product.Metadata"
-//   });
-//   return {
-//     title: t("title", { name: product.name }),
-//     description: t("description", { name: product.name }),
-//     openGraph: {
-//       title: t("title", { name: product.name }),
-//       description: t("description", { name: product.name }),
-//       siteName: t("title", { name: product.name }),
-//       images: [
-//         {
-//           url: image,
-//           type: "image/png",
-//           width: 200,
-//           height: 200,
-//           secureUrl: image
-//         }
-//       ],
-//       type: "website",
-//       url: `${process.env.NEXT_PUBLIC_BASE_URL}/${locale}/tobacco/${slugBrand}/${product.compositeId}`,
-//       locale: locale === "uk" ? "uk_UA" : "ru_RU"
-//     }
-//   };
-// }
+export async function generateMetadata({
+  params
+}: {
+  params: { locale: "uk" | "ru"; tobaccoId: string };
+}) {
+  const locale = getLocale(params);
+  const { data } = await getQuery({
+    params,
+    query: GET_TOBACCO_PRODUCT_BY_COMPOSITE_ID_QUERY,
+    variables: {
+      compositeId: params.tobaccoId
+    }
+  });
+  console.log(data, 'data generateMetadata')
+  const product = data.products.data?.at(0).attributes;
+  console.log(product, 'data generateMetadata 1')
+  const image = product.previewImage.data.attributes.url;
+  console.log(image, 'data generateMetadata 2')
+  const slugBrand = product.brand.data.attributes.slug;
+  console.log(slugBrand, 'data generateMetadata 3')
+  const t = await getTranslations({
+    locale,
+    namespace: "Tobacco.Product.Metadata"
+  });
+  return {
+    title: t("title", { name: product.name }),
+    description: t("description", { name: product.name }),
+    openGraph: {
+      title: t("title", { name: product.name }),
+      description: t("description", { name: product.name }),
+      siteName: t("title", { name: product.name }),
+      images: [
+        {
+          url: image,
+          type: "image/png",
+          width: 200,
+          height: 200,
+          secureUrl: image
+        }
+      ],
+      type: "website",
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${locale}/tobacco/${slugBrand}/${product.compositeId}`,
+      locale: locale === "uk" ? "uk_UA" : "ru_RU"
+    }
+  };
+}

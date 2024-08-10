f360b82bb899a41ec4cdae3e1a7e9ff27ec0f86d
ZaharBerku <zaharberku@gmail.com>
2024-08-10 16:00:03 +0300

    fixed open gallery

--- app/src/compoents/organisms/Gallery/index.tsx
+++ app/src/compoents/organisms/Gallery/index.tsx
@@ -5,7 +5,7 @@ import { PaginationButton } from "@/compoents/molecules";
 import clsx from "clsx";
 import Image, { ImageProps } from "next/image";
 import "photoswipe/dist/photoswipe.css";
-import { FC, useCallback, useRef, useState } from "react";
+import { FC, forwardRef, useCallback, useRef, useState } from "react";
 import { Gallery as GalleryWrapper, Item } from "react-photoswipe-gallery";
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,72 +13,57 @@ import { Swiper, SwiperSlide } from "swiper/react";
 interface GalleryItemProps extends ImageProps {
   classNameWrapper?: string;
   isMainItem?: boolean;
+  ref?: any;
 }
 
 interface GalleryProps {
   images: any;
 }
 
-const GalleryItem: FC<GalleryItemProps> = ({
-  classNameWrapper,
-  isMainItem,
-  className,
-  ...props
-}) => {
+const GalleryItem: FC<GalleryItemProps> = forwardRef<
+  HTMLDivElement,
+  GalleryItemProps
+>(({ classNameWrapper, isMainItem, className, onClick, ...props }, ref) => {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <Item
-      key={`image_${props.src}`}
-      original={props.src as string}
-      thumbnail={props.src as string}
-      width={500}
-      height={500}
-      alt={props.alt}
+    <div
+      ref={ref}
+      onClick={onClick}
+      className={clsx(
+        "relative flex justify-center items-center border border-gray-300 rounded-3xl",
+        classNameWrapper,
+        {
+          "aspect-square": isMainItem,
+          "aspect-[4/3]": isLoading
+        }
+      )}
     >
-      {({ ref, open }) => (
+      {isLoading && (
         <div
-          ref={ref}
-          onClick={open}
-          className={clsx(
-            "relative flex justify-center items-center border border-gray-300 rounded-3xl",
-            classNameWrapper,
-            {
-              "aspect-square": isMainItem,
-              "aspect-[4/3]": isLoading
-            }
-          )}
+          className={clsx("absolute inset-0 flex justify-center items-center", {
+            "py-24": isMainItem,
+            "py-6": !isMainItem
+          })}
         >
-          {isLoading && (
-            <div
-              className={clsx(
-                "absolute inset-0 flex justify-center items-center",
-                {
-                  "py-24": isMainItem,
-                  "py-6": !isMainItem
-                }
-              )}
-            >
-              <Icon
-                type="SpinnerIcon"
-                className={isMainItem ? "w-24 h-24" : "w-16 h-16"}
-              />
-            </div>
-          )}
-          <Image
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            {...props}
-            className={clsx(className, "transition-all opacity-0", {
-              "opacity-100": !isLoading
-            })}
-            onLoad={() => setIsLoading(false)}
+          <Icon
+            type="SpinnerIcon"
+            className={isMainItem ? "w-24 h-24" : "w-16 h-16"}
           />
         </div>
       )}
-    </Item>
+      <Image
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        {...props}
+        className={clsx(className, "transition-all opacity-0", {
+          "opacity-100": !isLoading
+        })}
+        onLoad={() => setIsLoading(false)}
+      />
+    </div>
   );
-};
+});
 
 const Gallery: FC<GalleryProps> = ({ images }) => {
   const initialSelectImage = images.at(0);
@@ -156,25 +141,53 @@ const Gallery: FC<GalleryProps> = ({ images }) => {
               {images?.map((img: any, index: number) => {
                 return (
                   <SwiperSlide key={index}>
-                    <GalleryItem
-                      classNameWrapper="w-full h-full"
-                      className="rounded-3xl object-cover !static"
-                      key={index}
-                      src={img.attributes.url}
+                    <Item
+                      key={`image_${img.attributes.url}`}
+                      original={img.attributes.url as string}
+                      thumbnail={img.attributes.url as string}
+                      width={500}
+                      height={500}
                       alt={img.attributes?.alternativeText || "product"}
-                    />
+                    >
+                      {({ ref, open }) => (
+                        <GalleryItem
+                          classNameWrapper="w-full h-full"
+                          className="rounded-3xl object-cover !static"
+                          key={index}
+                          onClick={open}
+                          ref={ref}
+                          src={img.attributes.url}
+                          alt={img.attributes?.alternativeText || "product"}
+                        />
+                      )}
+                    </Item>
                   </SwiperSlide>
                 );
               })}
             </Swiper>
           </>
         ) : (
-          <GalleryItem
-            classNameWrapper="w-full md:max-w-[450px] aspect-square flex-1 h-auto rounded-3xl"
-            className="object-contain rounded-3xl"
-            src={initialSelectImage.attributes.url}
+          <Item
+            key={`image_${initialSelectImage.attributes.url}`}
+            original={initialSelectImage.attributes.url as string}
+            thumbnail={initialSelectImage.attributes.url as string}
+            width={500}
+            height={500}
             alt={initialSelectImage?.attributes?.alternativeText || "product"}
-          />
+          >
+            {({ ref, open }) => (
+              <GalleryItem
+                classNameWrapper="w-full md:max-w-[450px] aspect-square flex-1 h-auto rounded-3xl"
+                className="object-contain rounded-3xl"
+                src={initialSelectImage.attributes.url}
+                ref={ref}
+                onClick={open}
+                alt={
+                  initialSelectImage?.attributes?.alternativeText || "product"
+                }
+              />
+            )}
+          </Item>
         )}
       </GalleryWrapper>
     </div>

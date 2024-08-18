e1573fcbe9218c7db4f6e2142d66a2d822d02977
ZaharBerku <zaharberku@gmail.com>
2024-08-18 13:45:17 +0300

    return types

--- admin/src/api/type/content-types/type/lifecycles.js
+++ admin/src/api/type/content-types/type/lifecycles.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//   async beforeCreate(event) {
-//     if (!event.params.data.slugType && event.params.data.locale === "ru") {
-//       // @ts-ignore
-//       const latestCreatedType = await strapi.query("api::type.type").findOne({
-//         orderBy: { createdAt: "desc" },
-//         where: { locale: "uk" },
-//       });
-//       const slug = latestCreatedType.slug;
-//       event.params.data.slugType = slug;
-//     } else if (
-//       event.params.data.locale === "uk" &&
-//       !event.params.data.slugType
-//     ) {
-//       event.params.data.slugType = event.params.data.slug;
-//     }
-//   }
-// };
+module.exports = {
+  async beforeCreate(event) {
+    if (!event.params.data.slugType && event.params.data.locale === "ru") {
+      // @ts-ignore
+      const latestCreatedType = await strapi.query("api::type.type").findOne({
+        orderBy: { createdAt: "desc" },
+        where: { locale: "uk" },
+      });
+      const slug = latestCreatedType.slug;
+      event.params.data.slugType = slug;
+    } else if (
+      event.params.data.locale === "uk" &&
+      !event.params.data.slugType
+    ) {
+      event.params.data.slugType = event.params.data.slug;
+    }
+  },
+};

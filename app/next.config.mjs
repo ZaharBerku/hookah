bebf7004b06f9f6a17697cc7a9d5b55c20165be5
ZaharBerku <zaharberku@gmail.com>
2024-08-28 11:37:51 +0300

    hide some redirect

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -37,38 +37,19 @@ const nextConfig = {
   async redirects() {
     return [
       // Handle duplicate pages with 301 redirects
-      {
-        source: '/index.php',
-        destination: '/uk', // Canonical page
-        permanent: true, // 301 redirect
-      },
-      {
-        source: '/index.html',
-        destination: '/uk',
-        permanent: true,
-      },
-      {
-        source: '/default.aspx',
-        destination: '/uk',
-        permanent: true,
-      },
-      {
-        source: '/home',
-        destination: '/uk',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.hookahstore.com.ua' }],
-        destination: 'https://hookahstore.com.ua/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'hookahstore.com.ua' }],
-        destination: 'https://hookahstore.com.ua/:path*',
-        permanent: true,
-      },
+    
+      // {
+      //   source: '/:path*',
+      //   has: [{ type: 'host', value: 'www.hookahstore.com.ua' }],
+      //   destination: 'https://hookahstore.com.ua/:path*',
+      //   permanent: true,
+      // },
+      // {
+      //   source: '/:path*',
+      //   has: [{ type: 'host', value: 'hookahstore.com.ua' }],
+      //   destination: 'https://hookahstore.com.ua/:path*',
+      //   permanent: true,
+      // },
       {
         source: '/((?!uk|ru).*)',
         destination: '/uk',

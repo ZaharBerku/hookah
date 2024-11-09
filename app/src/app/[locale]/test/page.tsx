52b7069058caff31a186a85e1125396327986662
ZaharBerku <zaharberku@gmail.com>
2024-11-09 18:00:19 +0200

    hide request

--- app/src/app/[locale]/test/page.tsx
+++ app/src/app/[locale]/test/page.tsx
@@ -1,11 +1,11 @@
 import { Head } from "@/compoents/molecules";
 import { SectionFAQ } from "@/compoents/organisms/SectionFAQ";
 // import { HomePage } from "@/compoents/pages";
-import { GET_ALL_PRODUCTS_QUERY } from "@/query/schema";
+// import { GET_ALL_PRODUCTS_QUERY } from "@/query/schema";
 import { getTranslations } from "next-intl/server";
-import { notFound } from "next/navigation";
+// import { notFound } from "next/navigation";
 
-import { getQuery } from "@/lib/server";
+// import { getQuery } from "@/lib/server";
 import { getLocale } from "@/utils/helpers";
 
 export default async function Home({
@@ -15,16 +15,16 @@ export default async function Home({
 }) {
   const locale = getLocale(params);
   const t = await getTranslations({ locale, namespace: "Metadata" });
-  const { loading, error, data } = await getQuery({
-    params,
-    query: GET_ALL_PRODUCTS_QUERY,
-    variables: {
-      limit: 15,
-      discountLimit: 1
-    }
-  });
+  // const { loading, error, data } = await getQuery({
+  //   params,
+  //   query: GET_ALL_PRODUCTS_QUERY,
+  //   variables: {
+  //     limit: 15,
+  //     discountLimit: 1
+  //   }
+  // });
 
-  if (error) notFound();
+  // if (error) notFound();
 
   const structuredData = {
     "@context": "https://schema.org",

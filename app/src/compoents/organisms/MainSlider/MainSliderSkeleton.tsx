49ae7e94641047d3782f9f32883a24212b424875
ZaharBerku <zaharberku@gmail.com>
2024-11-14 19:01:25 +0200

    return css

--- app/src/compoents/organisms/MainSlider/MainSliderSkeleton.tsx
+++ app/src/compoents/organisms/MainSlider/MainSliderSkeleton.tsx
@@ -4,10 +4,10 @@ const MainSliderSkeleton = () => {
   return (
     <section className="flex gap-10 w-full relative">
       <div className="hidden md:block relative max-w-74 w-full">
-        <Skeleton className="h-48 w-full rounded-lg" />
+        <Skeleton className="h-72 w-full rounded-lg" />
       </div>
       <div className="flex-1">
-        <Skeleton className="h-48 w-full rounded-lg mb-4" />
+        <Skeleton className="aspect-[8/3] w-full rounded-lg mb-4" />
       </div>
     </section>
   );

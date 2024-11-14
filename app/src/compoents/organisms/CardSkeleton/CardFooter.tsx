abaa888b15746ca500c4800a471c1b51a9d513b7
ZaharBerku <zaharberku@gmail.com>
2024-11-14 19:38:23 +0200

    fix button skeleton

--- app/src/compoents/organisms/CardSkeleton/CardFooter.tsx
+++ app/src/compoents/organisms/CardSkeleton/CardFooter.tsx
@@ -1,12 +1,13 @@
-import { FC } from "react";
 import { Skeleton } from "@nextui-org/skeleton";
-
+import { FC } from "react";
 
 const CardFooter: FC = () => {
   return (
     <div className="flex w-full gap-2 md:gap-4">
-      <Skeleton className={"rounded-9xl h-10 w-full max-w-14 md:max-w-23"}/>
-      <Skeleton className={"rounded-9xl h-8 md:h-10 w-full"}/>
+      <Skeleton
+        className={"rounded-md h-11 md:h-12 w-full max-w-14 md:max-w-23"}
+      />
+      <Skeleton className={"rounded-md h-11 md:h-12 w-full"} />
     </div>
   );
 };

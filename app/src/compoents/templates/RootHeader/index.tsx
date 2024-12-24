0dc1a367ed69899d89676056abab26b61b01b175
ZaharBerku <zaharberku@gmail.com>
2024-12-24 23:46:16 +0200

    change position for safari

--- app/src/compoents/templates/RootHeader/index.tsx
+++ app/src/compoents/templates/RootHeader/index.tsx
@@ -20,25 +20,27 @@ const RootHeader: FC<RootHeaderProps> = ({ isCloseBanner }) => {
     <Header className="w-full bg-white sticky top-0 md:bg-black flex flex-col justify-center items-center z-50">
       {!isCloseBanner && <Banner />}
       <SnowEffect />
-      <Wrapper className="w-full box-border mx-5 relative">
-        <div className="flex items-center gap-2 md:gap-8 border-b md:border-none justify-between py-4">
+      <div className="w-full flex justify-center items-center relative">
         <Icon
-            type="NewYearTreeLeftIcon"
-            className="fixed z-5 -left-10 xl:left-0 pointer-events-none"
-          />
-          <Logo classes={{ link: "hidden md:block" }} />
-          <Catalog />
-          <Sidebar isCloseBanner={isCloseBanner} />
-          <Logo type="LogoIcon" classes={{ link: "block md:hidden" }} />
-          <MainSearch />
-          <SwitchLanguage className="hidden md:block" />
-          <ShoppingCart />
-          <Icon
-            type="NewYearTreeRightIcon"
-            className="fixed z-5 right-0 md:-right-5 xl:right-0 w-14 md:w-20 pointer-events-none"
-          />
-        </div>
-      </Wrapper>
+          type="NewYearTreeLeftIcon"
+          className="absolute z-5 -left-10 xl:left-0 pointer-events-none"
+        />
+        <Wrapper className="w-full box-border relative">
+          <div className="flex items-center gap-2 md:gap-8 border-b md:border-none justify-between py-4">
+            <Logo classes={{ link: "hidden md:block" }} />
+            <Catalog />
+            <Sidebar isCloseBanner={isCloseBanner} />
+            <Logo type="LogoIcon" classes={{ link: "block md:hidden" }} />
+            <MainSearch />
+            <SwitchLanguage className="hidden md:block" />
+            <ShoppingCart />
+          </div>
+        </Wrapper>
+        <Icon
+          type="NewYearTreeRightIcon"
+          className="absolute z-5 right-0 md:-right-5 xl:right-0 w-14 md:w-20 pointer-events-none"
+        />
+      </div>
     </Header>
   );
 };

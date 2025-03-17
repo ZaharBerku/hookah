b6295112bb20e9b8a1ee86ad62a22f5a6f6033a9
ZaharBerku <zaharberku@gmail.com>
2025-03-17 19:00:46 +0200

    added unoptimized attribute to image in CardHeader

--- app/src/componets/organisms/Card/CardHeader.tsx
+++ app/src/componets/organisms/Card/CardHeader.tsx
@@ -22,6 +22,7 @@ const CardHeader: FC<CardHeaderProps> = ({ image }) => {
           "opacity-100": !isLoading
         })}
         loading="lazy"
+        unoptimized
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         src={image?.src || "/images/avatar.png"}
         alt={image?.alt || "product"}

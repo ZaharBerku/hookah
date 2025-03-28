625c80f0798a7d8eb631ec77250be8a85c9a017c
ZaharBerku <zaharberku@gmail.com>
2025-03-28 11:50:19 +0200

    refactor: simplify WrapperActionsProduct component by removing unused click handler

--- app/src/hoc/WrapperActionsProduct/index.tsx
+++ app/src/hoc/WrapperActionsProduct/index.tsx
@@ -1,24 +1,11 @@
-import { MouseEvent, PropsWithChildren, FC } from "react";
+import type { PropsWithChildren, FC } from "react";
 
 interface WrapperActionsProductProps extends PropsWithChildren {}
 
 const WrapperActionsProduct: FC<WrapperActionsProductProps> = ({
   children
 }) => {
-  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
-    const cardElement = (event.target as HTMLDivElement)?.closest(
-      "[data-card]"
-    );
-    const colorElement = (event.target as HTMLDivElement)?.closest(
-      "[data-color]"
-    );
-  };
-
-  return (
-    <div onClick={handleClick}>
-      {children}
-    </div>
-  );
+  return <div className="w-full">{children}</div>;
 };
 
 export { WrapperActionsProduct };

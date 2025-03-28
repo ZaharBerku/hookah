813ca400ae0b50db4cb71e74d2d73a3fe64a5913
ZaharBerku <zaharberku@gmail.com>
2025-03-28 07:07:12 +0200

    feat: add mobile image for FirstSlide component and adjust visibility classes

--- app/src/componets/organisms/MainSlider/FirstSlide.tsx
+++ app/src/componets/organisms/MainSlider/FirstSlide.tsx
@@ -12,7 +12,16 @@ const FirstSlide = () => {
         unoptimized
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         alt={"slider-pics-1"}
-        className="rounded-lg !static object-contain"
+        className="rounded-lg !hidden md:!block !static object-contain"
+      />
+      <Image
+        src={"/images/slide-mobile-1.webp"}
+        fill
+        loading="lazy"
+        unoptimized
+        sizes="(max-width: 768px) 100vw, 0vw"
+        alt={"slider-pics-1"}
+        className="rounded-lg !block md:!hidden !static object-contain"
       />
     </Link>
   );
